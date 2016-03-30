a12ac36a44b38cc3bb6acb5728b8ecd8f8172e63
Serdar Dogruyol <dogruyolserdar@gmail.com>
2016-03-30 16:41:18 +0300

    Add Kemal

--- kemal/app/src/app.cr
+++ kemal/app/src/app.cr
@@ -1,1 +1,15 @@
 require "kemal"
+
+logging false
+
+get "/" do |env|
+  title = env.params.query["title"]
+
+  members = [
+    { name: "Serdar Dogruyol" }
+  ]
+
+  render "src/views/index.ecr"
+end
+
+Kemal.run
